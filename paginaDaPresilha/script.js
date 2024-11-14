e573dc8094d0b42bab6edd364a9e1120628803d1
karinapagnani <159541094+karinapagnani@users.noreply.github.com>
2024-11-14 16:55:58 -0300

    fznd

--- paginaDaPresilha/script.js
+++ paginaDaPresilha/script.js
@@ -1,8 +1,19 @@
 function activateOpcao(selectedOpcao) {
-    // Remove a classe "active" de todas as abas
-    document.querySelectorAll('.opcao').forEach(tab => {
-          tab.classList.remove('active');
-            });
-    // Adiciona a classe "active" à aba clicada
-    selectedOpcao.classList.add('active');
+  // Remove a classe "active" de todas as abas
+  document.querySelectorAll('.opcao').forEach(tab => {
+        tab.classList.remove('active');
+          });
+  // Adiciona a classe "active" à aba clicada
+  selectedOpcao.classList.add('active');
 }
+/*
+function alterarVisibilidade() {
+  // Obtém os divs pelo ID
+  const primeiro = document.getElementById('primeiro');
+  const segundo = document.getElementById('segundo');
+
+  // Define div2 como invisível e div1 como visível
+  primeiro.style.display = 'block';
+  segundo.style.display = 'nome';
+}
+*/
